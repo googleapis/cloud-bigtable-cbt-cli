b99e8a3b7f366e67c1c2ad10619e88d5eb0c2486
Eric Schmidt <erschmid@google.com>
2022-02-01 10:59:39 -0800

    fix: go generate now creates docs

--- cbtconfig.go
+++ cbtconfig.go
@@ -14,8 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package cbtconfig encapsulates common code for reading configuration from .cbtrc and gcloud.
 package main
+
+// Package cbtconfig encapsulates common code for reading configuration from .cbtrc and gcloud.
 
 import (
 	"bufio"
